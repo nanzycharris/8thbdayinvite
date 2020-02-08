21fd1d2602daa797a0095ac24b9d2a6b1d556f18
Nancy Harris <nanzyharris@gmail.com>
2020-02-08 00:33:44 -0600

    changes to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -83,8 +83,8 @@ $(document).ready(function () {
     // Random questions 
     // Display the question and answer choices
     function displayQuestion() {
-        // Random index in array 
-        index = Math.floor(Math.random() * options.length);
+        // Questions display in the order they are written
+        index = Math.floor(i < options.length);
         pick = options[index];
 
         $("#questionblock").html("<h2>" + pick.question + "</h2>");
@@ -126,17 +126,16 @@ $(document).ready(function () {
             $("#answerblock").empty();
             timer = 25;
 
-            // Final score
-            if ((wrongCount + correctCount + unanswerCount) === qCount) {
+            // Ending the game winning or losing
+            if (((wrongCount + unanswerCount) < correctCount) && ((wrongCount + unanswerCount + correctCount) === qCount)) {
                 $("#questionblock").empty();
-                $("#questionblock").html("<h3>Great job! You're a winner and we want you to be our guest!<br><br> Here are the details to Teresa's 8th Birthday Party: <br><br>Date: Sunday, March 8th, 2020 at 2:30 p.m. <br><br>Place: Build-a-Bear at Galleria Mall <br><br>Afterwards, join us downstairs for pizza and cake at Lucciano's Pizzeria. <br><br>In lieu of presents, please make a gift to Teresa's college fund.<br><br> To donate, click here: <a href>www.ugift529.com</a href> and use the code N7W-N5Y <br><br>We look forward to seeing you there!</h3>");
-                // $("#answerblock").append("<h4>Correct answers: " + correctCount + "</h4>");
-                // $("#answerblock").append("<h4>Wrong answers: " + wrongCount + "</h4>");
-                // $("#answerblock").append("<h4>No answer: " + unanswerCount + "</h4>");
-                // $("#reset").show();
-                // correctCount = 0;
-                // wrongCount = 0;
-                // unanswerCount = 0;
+                $("#questionblock").html("<h3>Great job! You're a winner and we want you to be part of our rebellion!<br><br>Come join the resistanceHere are the details to Teresa's 8th Birthday Party: <br><br>Date: Sunday, March 8th, 2020 at 2:30 p.m. <br><br>Place: Build-a-Bear at Galleria Mall <br><br>Afterwards, join us downstairs for pizza and cake at Lucciano's Pizzeria. <br><br>In lieu of presents, please make a gift to Teresa's college fund.<br><br> To donate, click here: <a href>www.ugift529.com</a href> and use the code N7W-N5Y <br><br>Please RSVP via text message to Nancy<br><br>We look forward to seeing you there!</h3>");
+                $("#timeleft").hide();
+            } else if (((wrongCount + unanswerCount + correctCount) === qCount) && (wrongCount + unanswerCount) > correctCount) {
+                $("#questionblock").empty();
+                $("#questionblock").html("<h3>Sorry... you need more correct answers to unlock <br> <br> all the details to Teresa's birthday party <br> <br>Please try again by clicking the Restart button.</h3>");
+                $("#timeleft").hide();
+                $("#reset").show();
             } else {
                 runTimer();
                 displayQuestion();
@@ -144,7 +143,7 @@ $(document).ready(function () {
         }, 2500);
     }
 
-    $("#reset").on("click", function () {
+    function restart() {
         $("#reset").hide();
         $("#answerblock").empty();
         $("#questionblock").empty();
@@ -153,6 +152,17 @@ $(document).ready(function () {
         }
         runTimer();
         displayQuestion();
+    }
+
+    $("#reset").on("click", function () {
+        // $("#reset").hide();
+        // $("#answerblock").empty();
+        // $("#questionblock").empty();
+        // for (var i = 0; i < holder.length; i++) {
+        //     options.push(holder[i]);
+        // }
+        // runTimer();
+        // displayQuestion();
+        restart()
     })
 })
-
